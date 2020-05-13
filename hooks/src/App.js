9cfb01c66d8cd36b4a13329b711c515c94459b20
Rory <coderory@gmail.com>
2020-05-13 11:24:24 +0200

    2

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+
   return (
-    <div >
-      test
+    <div className="name">
+    
+    <p>My name is {name}</p>
+    <div>
+      <input type="text"/>
+      <button>Submit</button>
+      <p>Hey there, {name}</p>
+    </div>  
     </div>
   );
 }
